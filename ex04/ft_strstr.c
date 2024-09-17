8be2498e045578615a42b07e7ab67b08376218c3
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-17 10:42:08 +0200

    feat(ex04): implement

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 10:20:59 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/17 10:42:02 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (*to_find == '\0')
+		return (str);
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[0])
+		{
+			while(str[i + j] == to_find[j])
+				j++;
+			if (to_find[j] == '\0')
+				return (str + (i * sizeof(char)));
+		}
+		i++;
+	}
+	return NULL;
+}
+
+// int	main(void)
+// {
+// 	printf("OG: %s\n", strstr("Hello, world!", "world"));
+// 	printf("CU: %s\n", ft_strstr("Hello, world!", "world"));
+// 	printf("OG: %s\n", strstr("Hello, world!", ""));
+// 	printf("CU: %s\n", ft_strstr("Hello, world!", ""));
+// 	printf("OG: %s\n", strstr("Hello, world!", "42"));
+// 	printf("CU: %s\n", ft_strstr("Hello, world!", "42"));
+// }
