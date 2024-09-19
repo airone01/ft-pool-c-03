2bba075253ebb205d61d5c7cb19c7fa6181a5743
elagouch <elagouch@42>
2024-09-19 13:22:55 +0200

    fix(ex05): commit stuff

--- ex05/ft_strlcat.c
+++ ex05/ft_strlcat.c
@@ -11,8 +11,10 @@
 /* ************************************************************************** */
 
 // #include <stdio.h>
+// #include <string.h>
+// #include <bsd/string.h>
 
-int	ft_strlen(char *str)
+unsigned int	ft_strlen(char *str)
 {
 	if (*str == '\0')
 		return (1);
@@ -21,37 +23,58 @@ int	ft_strlen(char *str)
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int				dest_len;
-	int				src_len;
+	unsigned int	dest_len;
+	unsigned int	src_len;
 	unsigned int	i;
 
 	dest_len = ft_strlen(dest);
 	src_len = ft_strlen(src);
 	i = 0;
-	while (i < (size + 2) && i < (unsigned int)(dest_len + src_len))
+	if (size <= dest_len)
+		return (dest_len + size);
+	while (i < (size - dest_len - 1) && src[i] != '\0')
 	{
-		if (i > (unsigned int) dest_len)
-		{
-			dest[i - 2] = src[i - dest_len - 1];
-		}
+		dest[i + dest_len] = src[i];
 		i++;
 	}
-	dest[i - 2] = '\0';
+	dest[i + dest_len] = '\0';
 	return (ft_strlen(dest) - 1);
 }
 
 // int	main(void)
 // {
-// 	char	*src = "world!";
-// 	char	*src2 = "world!";
-// 	char	dest[500] = "SALUT COMMENT CA VA ";
-// 	char	dest2[500] = "Hello, ";
-// 	int	rest;
+// 	char dest11[500] = " ";
+// 	char dest12[500] = " ";
+// 	char dest21[500] = "Hello,";
+// 	char dest22[500] = "Hello,";
+// 	char dest31[500] = "";
+// 	char dest32[500] = "";
+// 	char *src1 = " ";
+// 	char *src2 = " world!";
+// 	char *src3 = "";
 //
-// 	printf ("### B4\nsrc: %s\ndest: %s\n", src, dest);
-// 	rest = ft_strlcat(dest, src, 100);
-// 	printf ("### @R\nsrc: %s\ndest: %s\nrest: %d\n", src, dest, rest);
-// 	printf ("### B4\nsrc: %s\ndest: %s\n", src2, dest2);
-// 	rest = ft_strlcat(dest2, src2, 12);
-// 	printf ("### @R\nsrc: %s\ndest: %s\nrest: %d\n", src2, dest2, rest);
+// 	// Our function
+// 	int res11 = ft_strlcat(dest11, src1, 5);
+// 	int res21 = ft_strlcat(dest21, src2, 0);
+// 	int res31 = ft_strlcat(dest31, src3, 100);
+// 	// Original
+// 	int res12 = strlcat(dest12, src1, 5);
+// 	int res22 = strlcat(dest22, src2, 0);
+// 	int res32 = strlcat(dest32, src3, 100);
+//
+// 	// Assert differences
+// 	printf("'%s' : '%s'\n", dest11, dest12);
+// 	if (strcmp(dest11, dest12) != 0)
+// 		return printf("dest11-12");
+// 	if (strcmp(dest21, dest22) != 0)
+// 		return printf("dest21-22");
+// 	if (strcmp(dest31, dest32) != 0)
+// 		return printf("dest31-32");
+// 	// Assert results
+// 	if (res11 == res12)
+// 		return printf("res11-12");
+// 	if (res21 == res22)
+// 		return printf("res21-22");
+// 	if (res31 == res32)
+// 		return printf("res31-32");
 // }
