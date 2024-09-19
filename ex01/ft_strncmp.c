799ab7762b2b422f5f4ab46187245439c6d3f5ec
elagouch <elagouch@42>
2024-09-19 18:34:07 +0200

    fix(ex01): patch

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -10,39 +10,42 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <stdio.h>
-// #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
 
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i <= n)
-		i++;
-	if (s1[i] == '\0' && s2[i] == '\0')
+	if (n == 0)
 		return (0);
-	else
-		return (s1[i] - s2[i]);
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i] || !s1[i] || !s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
 
-// int	main(void)
-// {
-// 	printf("###\ncmp og: %d\n", strncmp("ABC", "ABC", 100));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABC", "ABC", 100));
-// 	//
-// 	printf("###\ncmp og: %d\n", strncmp("ABC", "ABC", 2));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABC", "ABC", 2));
-// 	//
-// 	printf("###\ncmp og: %d\n", strncmp("ABC", "AB", 100));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABC", "AB", 100));
-// 	//
-// 	printf("###\ncmp og: %d\n", strncmp("ABA", "ABZ", 100));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABA", "ABZ", 100));
-// 	//
-// 	printf("###\ncmp og: %d\n", strncmp("ABCEEFGH", "ABCDEFGH", 5));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABCEEFGH", "ABCDEFGH", 5));
-// 	//
-// 	printf("###\ncmp og: %d\n", strncmp("ABCDEFGHIJK", "ONMLKJIHGFE", 8));
-// 	printf("cmp cu: %d\n", ft_strncmp("ABCDEFGHIJK", "ONMLKJIHGFE", 8));
-// }
+int	main(void)
+{
+	printf("###\ncmp og: %d\n", strncmp("ABC", "ABC", 100));
+	printf("cmp cu: %d\n", ft_strncmp("ABC", "ABC", 100));
+	//
+	printf("###\ncmp og: %d\n", strncmp("ABC", "ABC", 2));
+	printf("cmp cu: %d\n", ft_strncmp("ABC", "ABC", 2));
+	//
+	printf("###\ncmp og: %d\n", strncmp("ABC", "AB", 100));
+	printf("cmp cu: %d\n", ft_strncmp("ABC", "AB", 100));
+	//
+	printf("###\ncmp og: %d\n", strncmp("ABA", "ABZ", 100));
+	printf("cmp cu: %d\n", ft_strncmp("ABA", "ABZ", 100));
+	//
+	printf("###\ncmp og: %d\n", strncmp("ABCEEFGH", "ABCDEFGH", 5));
+	printf("cmp cu: %d\n", ft_strncmp("ABCEEFGH", "ABCDEFGH", 5));
+	//
+	printf("###\ncmp og: %d\n", strncmp("ABCDEFGHIJK", "ONMLKJIHGFE", 8));
+	printf("cmp cu: %d\n", ft_strncmp("ABCDEFGHIJK", "ONMLKJIHGFE", 8));
+}
